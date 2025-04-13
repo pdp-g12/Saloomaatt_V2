e61a4184d42fa6eb47cf58a6a6fd6c5e927d29f0
wwlinuxoff@gmail.com <wwlinuxoff@gmail.com>
2025-04-13 14:38:51 +0500

    Salomat V2 Project yaratildi

---
+++ Saloomaatt_V2/MathV2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class MathV2
+    {
+        public static double qoldiqliBolish(double a, double b)
+        {
+            return a % b;
+        }
+    }
+}
---
+++ Saloomaatt_V2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Calculator;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Console Calculator: \n\n1. Qo'shish \n2. Ayirish \n3. Ko'paytirish \n4. Bo'lish \n5. Qoldiqli bo'lish");
+
+        int select = Convert.ToInt32(Console.ReadLine());
+
+        switch (select)
+        {
+            case 1:
+                //code
+                break;
+            case 2:
+                // code
+                break;
+            case 3:
+                //code
+                break;
+            case 4:
+                // code
+                break;
+            case 5:
+                Console.Write("1- Son kiritin! - ");
+                double number1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("2- Son kiritin! - ");
+                double number2 = Convert.ToDouble(Console.ReadLine());
+
+                MathV2.qoldiqliBolish(number1, number2);
+                break;
+        }
+    }
+}
